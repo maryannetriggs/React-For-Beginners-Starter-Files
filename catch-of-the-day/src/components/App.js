07fbe8783e8e5c422c501da67454f5e8e7d8af58
Mary-Anne Triggs <maryannetriggs@gmail.com>
2020-08-04 16:15:27 +0100

    lesson 22 complete

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -7,6 +7,8 @@ import Inventory from './Inventory'
 import sampleFishes from '../sample-fishes' 
 import Fish from './Fish'
 
+import '../css/style.css'
+
 
 class App extends React.Component {
   state = {
@@ -38,7 +40,7 @@ class App extends React.Component {
 
 
   addFish = fish => {
-    // 1. Take a copy of state
+    // 1. Take a copy of the current state
     const fishes = { ...this.state.fishes }
     // 2. Add our new fish to the fishes variable
     fishes[`fish${Date.now()}`] = fish
@@ -57,19 +59,40 @@ class App extends React.Component {
     this.setState({ fishes })
   }
 
+  deleteFish = (key) => {
+    // 1. Take a copy of the current state
+    const fishes = { ...this.state.fishes }
+
+    // 2. Update the fish
+    fishes[key] = null
+
+    // 3. Update state
+    this.setState({ fishes })
+  }
+
   loadSampleFishes = () => {
     this.setState({ fishes: sampleFishes })
   }
 
   addToOrder = key => {
-    // 1. Take a copy of state
+    // 1. Take a copy of the current state
     const order = { ...this.state.order }
     // 2. Either add to order or update the number in order 
     order[key] = order[key] + 1 || 1
     // 3. Update state
     this.setState({ order })
   }
-  
+
+  removeFromOrder = key => {
+    // 1. Take a copy of the current state
+    const order = { ...this.state.order }
+
+    // 2. Remove item from the order
+    delete order[key]
+
+    // 3. Update state
+    this.setState({ order })
+  }
 
   render() {
     const { fishes, order } = this.state
@@ -90,10 +113,12 @@ class App extends React.Component {
         <Order 
           fishes={fishes}
           order={order}
+          removeFromOrder={this.removeFromOrder}
         />
         <Inventory 
           addFish={this.addFish}
           updateFish={this.updateFish}
+          deleteFish={this.deleteFish}
           loadSampleFishes={this.loadSampleFishes}
           fishes={fishes}
         />
--- catch-of-the-day/src/sample-fishes.js
+++ catch-of-the-day/src/sample-fishes.js
@@ -4,8 +4,8 @@ const fishes = {
     name: 'Pacific Halibut',
     image: '/images/hali.jpg',
     desc:
-      'Everyone’s favorite white fish. We will cut it to the size you need and ship it.',
-    price: 1724,
+      'Everyone’s favourite white fish. We will cut it to the size you need and ship it.',
+    price: 2724,
     status: 'available'
   },
 
@@ -14,7 +14,7 @@ const fishes = {
     image: '/images/lobster.jpg',
     desc:
       'These tender, mouth-watering beauties are a fantastic hit at any dinner party.',
-    price: 3200,
+    price: 6150,
     status: 'available'
   },
 
@@ -22,8 +22,8 @@ const fishes = {
     name: 'Sea Scallops',
     image: '/images/scallops.jpg',
     desc:
-      'Big, sweet and tender. True dry-pack scallops from the icey waters of Alaska. About 8-10 per pound',
-    price: 1684,
+      'Big, sweet and tender. True dry-pack scallops from the icey waters of Alaska. About 20 per Kg',
+    price: 3200,
     status: 'unavailable'
   },
 
@@ -31,8 +31,8 @@ const fishes = {
     name: 'Mahi Mahi',
     image: '/images/mahi.jpg',
     desc:
-      'Lean flesh with a mild, sweet flavor profile, moderately firm texture and large, moist flakes. ',
-    price: 1129,
+      'Lean flesh with a mild, sweet flavour profile, moderately firm texture and large, moist flakes. ',
+    price: 2119,
     status: 'available'
   },
 
@@ -41,7 +41,7 @@ const fishes = {
     image: '/images/crab.jpg',
     desc:
       'Crack these open and enjoy them plain or with one of our cocktail sauces',
-    price: 4234,
+    price: 8456,
     status: 'available'
   },
 
@@ -49,16 +49,16 @@ const fishes = {
     name: 'Atlantic Salmon',
     image: '/images/salmon.jpg',
     desc:
-      'This flaky, oily salmon is truly the king of the sea. Bake it, grill it, broil it...as good as it gets!',
-    price: 1453,
+      'This flaky, oily salmon is truly the king of the sea. Bake it, grill it... As good as it gets!',
+    price: 3000,
     status: 'available'
   },
 
   fish7: {
     name: 'Oysters',
     image: '/images/oysters.jpg',
-    desc: 'A soft plump oyster with a sweet salty flavor and a clean finish.',
-    price: 2543,
+    desc: 'A soft plump oyster with a sweet, salty flavour and a clean finish.',
+    price: 4835,
     status: 'available'
   },
 
@@ -66,8 +66,8 @@ const fishes = {
     name: 'Mussels',
     image: '/images/mussels.jpg',
     desc:
-      'The best mussels from the Pacific Northwest with a full-flavored and complex taste.',
-    price: 425,
+      'The best mussels from the Pacific Northwest with a full-flavoured and complex taste.',
+    price: 900,
     status: 'available'
   },
 
@@ -75,8 +75,8 @@ const fishes = {
     name: 'Jumbo Prawns',
     image: '/images/prawns.jpg',
     desc:
-      'With 21-25 two bite prawns in each pound, these sweet morsels are perfect for shish-kabobs.',
-    price: 2250,
+      'With 50-55 two-bite prawns in each Kg, these sweet morsels are perfect for kebabs.',
+    price: 2550,
     status: 'available'
   }
 }
